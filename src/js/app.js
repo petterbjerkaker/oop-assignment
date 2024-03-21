e1ed9c6a1f26018a3275b94f62031d963090504b
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-03-21 15:45:43 +0100

    Add ID for both classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,18 @@
 class PrescriptionMedicine {
-	constructor(name, manufacturer, id, expiration, type, quantity){
+	constructor(name, manufacturer, expiration, type, quantity){
 		this.name = name;
 		this.manufacturer = manufacturer;
-		this.id = id;
 		this.expiration = expiration;
 		this.type = type;
 		this.quantity = quantity;
-		this.ID = Date.now()
+		this.ID = Date.now();
 	}
 }
 
 class OTCMedicine extends PrescriptionMedicine {
-	
+	constructor(name, manufacturer, expiration, type, quantity){
+		super(name, manufacturer, expiration, type, quantity);
+		this.ID = Date.now();
+
+	}
 }
